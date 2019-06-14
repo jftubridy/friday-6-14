f64f786fe2e8a770d9d6cb1dd3d24626fc98c27d
Joseph Tubridy <jftubridy@gmail.com>
2019-06-14 08:28:51 -0700

    initial commit with bare bones outline

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+// Business Logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// User Interface Logic
+$(function(){
+  var number;
+  $("#userInput").submit(function(event){
+  number = parseInt($("input#number").val());
+
+  if (number == 3) {
+
+} else if (number == 2) {
+
+} else if (number == 1) {
+
+} else {
+
+}
+event.preventDefault();
+})
+});

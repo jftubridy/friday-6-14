3b43cb7f247a13b9c86f25a1e9a57097b89a6436
Joseph Tubridy <jftubridy@gmail.com>
2019-06-14 09:04:54 -0700

    skip basic logic currently, to test button appending values

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,38 @@
 // Business Logic
+function HAL(number) {
 
+  if(isNaN(number)) {
+    alert("Please enter a valid number");
+  } else if (number <=0) {
+    alert("Please enter a number that is higher than 0");
+  } else {
+    for (var index = num1; num1 <= num2; num1 += incr) {
 
+    }if (number == 3) {
 
+    } else if (number == 2) {
 
+    } else if (number == 1) {
 
+    } else {
 
-
-
-
-
-
-
-
-
-
-// User Interface Logic
-$(function(){
-  var number;
-  $("#userInput").submit(function(event){
-  number = parseInt($("input#number").val());
-
-  if (number == 3) {
-
-} else if (number == 2) {
-
-} else if (number == 1) {
-
-} else {
+    }
 
 }
-event.preventDefault();
-})
+}
+
+
+
+
+
+var number;
+// User Interface Logic
+$(document).ready(function(){
+  $("form#userInput").submit(function(event){
+    event.preventDefault();
+  number = parseInt($("input#number").val());
+
+  response = (number);
+  $("#response").append("<li>"+response+"</li>");
+});
 });

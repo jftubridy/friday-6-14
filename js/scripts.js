85bfc50c25d44ac8ef70759b65f1ec37941abca2
Joseph Tubridy <jftubridy@gmail.com>
2019-06-14 13:19:40 -0700

    add finally working javascript logic, and I think I understand for loops a bit better now

--- js/scripts.js
+++ js/scripts.js
@@ -3,41 +3,43 @@ var HAL = function(inputNumber) {
 var three = 3;
 var two = 2;
 var one = 1;
-debugger;
+// debugger;
   if(isNaN(inputNumber)) {
     alert("Please enter a valid number");
   } else if (inputNumber <=0) {
     alert("Please enter a number that is higher than 0");
   }
-  // (number.includes('3',0)) {
-   //   return "I'm Sorry, Dave, I'm afraid I can't do that.";
-    // } else if (number.includes('2',0)) {
-    //   return "Boop!";
-    // } else if (number.includes('1',0)) {
-    //   return "Beep!";
-    // } else {
-    //   return number;
-    // }
- alert(typeof(inputNumber));
-    numToString = inputNumber.toString();
-    alert(typeof(numToString));
+ //alert(typeof(inputNumber));
+    var numToString = inputNumber.toString();
+    //alert(typeof(numToString));
     var numArray = numToString.split('');
-    alert(typeof(numArray));
-    for (var index = 0; index <= numArray.length; index ++) {
-
-    }if (numArray[0] == 3) {
+    //alert(typeof(numArray));
+    for (index = 0; index < numArray.length;) {
+      if (numArray[index] == 3){
         return "I'm Sorry, Dave, I'm afraid I can't do that.";
-      } else if (numArray[0] == 2) {
-        return "Boop!";
-      } else if (numArray[0] == 1) {
-        return "Beep!";
-      } else {
-        return inputNumber;
+        break;
+      }  else {
+        index++;
       }
-
+    }
+    for (index = 0; index < numArray.length;) {
+      if (numArray[index] == 2) {
+        return "Boop!";
+        break;
+      } else {
+        index ++;
+      }
+    }
+    for (index = 0; index < numArray.length;) {
+      if (numArray[index] == 1) {
+        return "Beep!";
+        break;
+      } else {
+        index ++;
+      }
+  }
+  return inputNumber;
 }
-
-
 
 
 
@@ -47,19 +49,6 @@ $(document).ready(function(){
   $("form#userInput").submit(function(event){
     event.preventDefault();
     var inputNumber = parseInt($("input#number").val());
-    // var numArray = number.split('');
-    // for (var i = 0; i <= numArray.length ; i++){
-    //   if (three.includes(numArray[i])){
-    //     return "I'm Sorry, Dave, I'm afraid I can't do that.";
-    //   }else if (two.includes(numArray[i])){
-    //     return "Boop!";
-    //   }else if (one.includes(numArray[i])){
-    //     return "Beep!";
-    //   }else {
-    //     return (numArray.join(''));
-
-  //}
-
     var response = HAL(inputNumber);
   $("#response").append("<li>"+response+"</li>");
 

18ac1c0b82459cde0b7d6c314a312c53a05ba622
Joseph Tubridy <jftubridy@gmail.com>
2019-06-14 13:45:04 -0700

    add a small amount of styling, and harass the user slightly

--- js/scripts.js
+++ js/scripts.js
@@ -5,40 +5,37 @@ var two = 2;
 var one = 1;
 // debugger;
   if(isNaN(inputNumber)) {
-    alert("Please enter a valid number");
+    return("I Think you're drunk Dave.");
   } else if (inputNumber <=0) {
-    alert("Please enter a number that is higher than 0");
+    return("A number we can use Dave.");
   }
- //alert(typeof(inputNumber));
-    var numToString = inputNumber.toString();
-    //alert(typeof(numToString));
-    var numArray = numToString.split('');
-    //alert(typeof(numArray));
-    for (index = 0; index < numArray.length;) {
-      if (numArray[index] == 3){
-        return "I'm Sorry, Dave, I'm afraid I can't do that.";
-        break;
-      }  else {
-        index++;
-      }
+  var numToString = inputNumber.toString();
+  var numArray = numToString.split('');
+  for (index = 0; index < numArray.length;) {
+    if (numArray[index] == 3){
+      return " I'm Sorry, Dave, I'm afraid I can't do that.";
+      break;
+    }  else {
+      index++;
     }
-    for (index = 0; index < numArray.length;) {
-      if (numArray[index] == 2) {
-        return "Boop!";
-        break;
-      } else {
-        index ++;
-      }
-    }
-    for (index = 0; index < numArray.length;) {
-      if (numArray[index] == 1) {
-        return "Beep!";
-        break;
-      } else {
-        index ++;
-      }
   }
-  return inputNumber;
+  for (index = 0; index < numArray.length;) {
+    if (numArray[index] == 2) {
+      return 'Boop!';
+      break;
+    } else {
+      index ++;
+    }
+  }
+  for (index = 0; index < numArray.length;) {
+    if (numArray[index] == 1) {
+      return 'Beep!';
+      break;
+    } else {
+      index ++;
+    }
+  }
+  return '  '+inputNumber + '?';
 }
 
 
@@ -50,7 +47,7 @@ $(document).ready(function(){
     event.preventDefault();
     var inputNumber = parseInt($("input#number").val());
     var response = HAL(inputNumber);
-  $("#response").append("<li>"+response+"</li>");
+  $("#response").append("<li>"+response+"</li>"+' ');
 
 });
 });
